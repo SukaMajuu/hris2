586e7184feace55cd727b4f2bac53930e635ce15
Fahridanaa <fahridanaaa@gmail.com>
2025-04-20 18:11:55 +0700

    refactor(auth): Change LastName field to be nullable in AdminRegisterRequest for improved flexibility in user registration data management

--- apps/backend/internal/rest/dto/auth/admin_register_request.go
+++ apps/backend/internal/rest/dto/auth/admin_register_request.go
@@ -5,10 +5,10 @@ import (
 )
 
 type AdminRegisterRequest struct {
-	Email           string `json:"email" validate:"required,email"`
-	Password        string `json:"password" validate:"required,min=8"`
-	FirstName       string `json:"first_name" validate:"required"`
-	LastName        string `json:"last_name" validate:"required"`
+	Email           string  `json:"email" validate:"required,email"`
+	Password        string  `json:"password" validate:"required,min=8"`
+	FirstName       string  `json:"first_name" validate:"required"`
+	LastName        *string `json:"last_name"`
 
 	AgreeTerms      bool   `json:"agree_terms" validate:"required,eq=true"`
 }

11b7750a88998141f485d502d838616d76069228
aryachandrak <aryachandrak14@gmail.com>
2025-05-02 15:25:05 +0700

    sidebar belum jadi

--- apps/frontend/src/app/employee-management/page.tsx
+++ apps/frontend/src/app/employee-management/page.tsx
@@ -1,40 +1,83 @@
-import { SidebarFooter } from "@/components/ui/sidebar";
+"use client";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarGroup,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
 export function AppSidebar() {
-    return (
-      <SidebarProvider>
-        <Sidebar>
-          <SidebarHeader />
-          <SidebarContent />
-          <SidebarFooter>
+  return (
+    <SidebarProvider>
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
             <SidebarMenu>
-              <SidebarMenuItem>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <SidebarMenuButton>
-                      <User2 /> Username
-                      <ChevronUp className="ml-auto" />
-                    </SidebarMenuButton>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent
-                    side="top"
-                    className="w-[--radix-popper-anchor-width]"
-                  >
-                    <DropdownMenuItem>
-                      <span>Account</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <span>Billing</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <span>Sign out</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </SidebarMenuItem>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
             </SidebarMenu>
-          </SidebarFooter>
-        </Sidebar>
-      </SidebarProvider>
-    )
-  }
-  
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+    </SidebarProvider>
+  )
+}
+
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Employee Management</h1>
+      <AppSidebar />
+    </div>
+  );
+}

f563c7fd3ebf290e9f2888071ea72ee53ba0ed11
Fahridanaa <fahridanaaa@gmail.com>
2025-05-21 12:22:44 +0700

    feat(location): refactor location request DTOs by removing the old CreateLocationRequest and adding new validation and list request structures

--- apps/backend/internal/rest/dto/check-clock/location_request.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type CreateLocationRequest struct {
-	Name      string  `json:"name"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	RadiusM   int     `json:"radius_m"`
-}
